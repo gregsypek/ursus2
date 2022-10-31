122f0a6a0f88f03e8c3d9dde70ba0dd1512f1705
gregsypek <47268138+gregsypek@users.noreply.github.com>
2022-10-31 14:56:24 +0100

    add aboutview page

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<!-- main -->
+	<section class="my-5">
+		<div class="container">
+			<div class="d-flex row justify-content-center g-5">
+				<aside class="d-none d-lg-block col-3 me-5">
+					<div class="rounded">
+						<!-- <h3>URSUS</h3> -->
+						<img src="../assets/380-2.jpeg" alt="ursus 380-2" class="w-100 h-100" style="objectFit: cover" />
+					</div>
+				</aside>
+				<main class="row col-12 col-lg-8 gap-2 gap-lg-5 flex justify-content-between">	
+					<div class="col-12 col-lg-6">
+            <h3>About application:</h3>
+						<p>I've created this application only for learning purposes—all pictures and information I copied from the original URSUS website link below.<strong><a class="nav-link text-center py-3 " href="https://www.ursus.com/pl" style="color:red">https://www.ursus.com/pl.</a></strong>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eos facere quia, voluptate dicta recusandae quae? Odit omnis, nam fugit numquam odio laboriosam magnam, architecto maxime voluptatem aliquid quasi ipsam. Blanditiis voluptates ratione iste, repellat explicabo quas quisquam nam libero voluptate officia aliquam temporibus earum tenetur non sapiente rerum suscipit.</p>
+					</div>
+					<div class=" col-4 col-lg-md">			
+				
+              <h3>Stack:</h3>
+              <ul class="card-text py-3">
+							<li>MongoDB</li>
+							<li>Express</li>
+							<li>Vue</li>
+							<li>Node</li>
+						</ul>
+						
+						</h5>
+					</div>
+					<div class="col-4 col-lg-md">
+						<h3>Back-End</h3>
+						<ul class="card-text py-3">
+							<li>Node</li>
+							<li>Express</li>
+							<li>Express</li>
+							<li>MongoDB</li>
+						</ul>
+					</div>
+					<div class="col-md">
+						<h3>Front-End</h3>
+						<ul class="card-text py-3">
+							<li>VueJS</li>
+							<li>VueRouter</li>
+							<li>Axios</li>
+							<li>Bootstrap</li>
+							<li>Vuetify</li>
+						</ul>
+					</div>
+				</main>
+			</div>
+		</div>
+	</section>
 </template>
