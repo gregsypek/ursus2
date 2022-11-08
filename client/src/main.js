e52f9cf03e1bb8707dd02e18d1fc4a5050cbb887
gregsypek <47268138+gregsypek@users.noreply.github.com>
2022-11-08 18:05:41 +0100

    add a security measure to prevent saving a fake token to local storage

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import store from "./vuex/store";
+import axios from "axios";
 
 Vue.config.productionTip = false;
 
@@ -15,6 +16,19 @@ new Vue({
 			const userData = JSON.parse(userString);
 			this.$store.commit("SET_USER_DATA", userData);
 		}
+
+		//works wth verifyToken method to prevent from fake token
+		axios.interceptors.response.use(
+			(response) => response,
+			(error) => {
+				console.log(error.response);
+				if (error.response.status === 401) {
+					this.$router.push("/");
+					this.$store.dispatch("logout");
+				}
+				return Promise.reject(error);
+			}
+		);
 	},
 	vuetify,
 	render: (h) => h(App),
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const API = require("../controllers/api");
 const multer = require("multer");
-
+const jwt = require("jsonwebtoken");
 //multer middleware
 let storage = multer.diskStorage({
 	destination: function (req, file, cb) {
@@ -25,15 +25,25 @@ function verifyToken(req, res, next) {
 		const bearer = bearerHeader.split(" ");
 		const bearerToken = bearer[1];
 		req.token = bearerToken;
-		next();
+
+		// verifies token
+		jwt.verify(req.token, "the_secret_key", (err) => {
+			if (err) {
+				return res.sendStatus(401);
+			} else {		
+				next();
+			}
+		});
 	} else {
 		res.sendStatus(401);
 	}
 }
 
+
+
 router.get("/", API.fetchAllPost);
 router.get("/:id", API.fetchPostByID);
-router.post("/", upload, API.createPost);
+router.post("/", verifyToken, upload, API.createPost);
 router.post("/register", API.createUser);
 router.post("/login", API.loginUser);
 router.patch("/:id", upload, API.updatePost);
