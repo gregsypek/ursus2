b5a19781d4c0730acd759c7ef0ad6f281c520896
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-11-07 23:21:20 +0100

    k

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: ["vuetify"],
+  transpileDependencies: ["vuetify", 'my-dep',],
 	devServer: {
 		proxy: "http://localhost:5001",
 		// proxy: {
